4fab10fbd8b78c44aeb047b93aabe2689256f416
Yoshitaka <yoshitaka@ktgwya.jp>
2026-08-05 12:13:42 +0900

    tweak: encode buffer

--- src/optim/jpeg.rs
+++ src/optim/jpeg.rs
@@ -1,8 +1,6 @@
 use std::path::PathBuf;
 use image::ImageReader;
 use image::codecs::jpeg::JpegEncoder;
-use std::fs::File as FsFile;
-use std::io::BufWriter;
 
 use crate::app::App;
 
@@ -18,15 +16,12 @@ impl Jpeg {
         // 先にファイルを読み込んでおく
         let file_image = ImageReader::open(&path)?.decode()?;
 
-        // 諸々設定
-        let extension = path.extension().unwrap().to_str().unwrap();
-        let out_path = path.with_extension(extension);
-        let file = FsFile::create(&out_path)?;
-        let mut writer = BufWriter::new(file);
-        let mut encoder = JpegEncoder::new_with_quality(&mut writer, *app.jpeg_quality());
-
-        // 読み込んでおいたものを最適化して出力
-        encoder.encode_image(&file_image)?;
+        let mut buffer = Vec::new();
+        {
+            let mut encoder = JpegEncoder::new_with_quality(&mut buffer, *app.jpeg_quality());
+            encoder.encode_image(&file_image)?;
+        }
+        std::fs::write(path, &buffer)?;
 
         Ok(())
     }
