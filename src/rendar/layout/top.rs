d113b24b3d7b7aaca09d51a583d21f81878e35ec
Yoshitaka <yoshitaka@ktgwya.jp>
2026-08-05 21:55:57 +0900

    add: open and clear svg icon

--- src/rendar/layout/top.rs
+++ src/rendar/layout/top.rs
@@ -1,4 +1,7 @@
+use egui::Color32;
+
 use crate::file::open_files;
+use crate::rendar::assets::svg;
 
 /// 上部ボタンを表示
 /// * `ui` - UI
@@ -13,13 +16,17 @@ pub(crate) fn top_layout(
         ui.horizontal(|ui| {
             ui.label("File Drag & Drop");
 
-            // クリアボタンを右寄せ
+            // 開くボタンとクリアボタンを右寄せ
             ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                if ui.button("Clear").clicked() {
+                // クリアボタン
+                let clear_button = egui::Image::new(svg::CLEAR_ALL).max_height(18.0).tint(Color32::WHITE);
+                if ui.button(clear_button).clicked() {
                     files.clear();
                 }
 
-                if ui.button("Open").clicked() {
+                // 開くボタン
+                let open_button = egui::Image::new(svg::FOLDER_OPEN).max_height(18.0).tint(Color32::WHITE);
+                if ui.button(open_button).clicked() {
                     // ファイルダイアログを開くタイミングをずらす
                     *open_dialog = true;
                     ui.ctx().request_repaint();
