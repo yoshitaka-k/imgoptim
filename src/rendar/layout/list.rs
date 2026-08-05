8dd9bd7a2bb7f3c2b7c077045efdaa98d070400b
Yoshitaka <yoshitaka@ktgwya.jp>
2026-08-05 12:28:07 +0900

    refactor

--- src/rendar/layout/list.rs
+++ src/rendar/layout/list.rs
@@ -12,55 +12,44 @@ pub(crate) fn file_list(ui: &mut egui::Ui, files: &open_files::OpenFiles) {
     ui.spacing_mut().item_spacing.y = 0.0;
 
     for (index, path) in files.paths().iter().enumerate() {
-        match path.path().metadata() {
-            Ok(_) => {
-                let size = path.size() / 1024;
+        let size = path.size() / 1024;
 
-                match path.status() {
-                    OptimizeStatus::None => {
-                        ui.horizontal(|ui| {
-                            ui.label(path.file_name());
-                            ui.label(format!("({} KB)", size));
-                        });
-                    }
-                    OptimizeStatus::Optimizing => {
-                        ui.horizontal(|ui| {
-                            ui.label(text_color("⏳", Color32::YELLOW, None));
-                            ui.label(path.file_name());
-                            ui.label(format!("({} KB)", size));
-                        });
-                    }
-                    OptimizeStatus::Optimized => {
-                        ui.horizontal(|ui| {
-                            ui.label(text_color("✅", Color32::GREEN, None));
-                            ui.label(path.file_name());
-                            ui.label(format!("({} KB -> {} KB ", size, path.new_size() / 1024));
-
-                            let spacing = ui.spacing().item_spacing.x;
-
-                            // "%"と")"とスペースを0にしてテキストを表示
-                            ui.spacing_mut().item_spacing.x = 0.0;
-                            ui.label(text_color(&format!("{}%", path.percent()), Color32::GREEN, Some(12.0)));
-                            ui.label(")");
-
-                            // スペースを元に戻す
-                            ui.spacing_mut().item_spacing.x = spacing;
-                        });
-                    }
-                    OptimizeStatus::Error(e) => {
-                        ui.horizontal(|ui| {
-                            ui.label(text_color("❌", Color32::RED, None));
-                            ui.label(path.file_name());
-                            ui.label(text_color(e, Color32::RED, Some(11.0)));
-                        });
-                    }
+        ui.horizontal(|ui| {
+            match path.status() {
+                OptimizeStatus::None => {
+                    ui.label(path.file_name());
+                    ui.label(format!("({} KB)", size));
                 }
+                OptimizeStatus::Optimizing => {
+                    ui.label(text_color("⏳", Color32::YELLOW, None));
+                    ui.label(path.file_name());
+                    ui.label(format!("({} KB)", size));
+                }
+                OptimizeStatus::Optimized => {
+                    ui.label(text_color("✅", Color32::GREEN, None));
+                    ui.label(path.file_name());
+                    ui.label(format!("({} KB -> {} KB ", size, path.new_size() / 1024));
 
-                if index < files.paths().len() - 1 {
-                    ui.add(egui::Separator::default().spacing(4.0));
+                    let spacing = ui.spacing().item_spacing.x;
+
+                    // "%"と")"とスペースを0にしてテキストを表示
+                    ui.spacing_mut().item_spacing.x = 0.0;
+                    ui.label(text_color(&format!("{}%", path.percent()), Color32::GREEN, Some(12.0)));
+                    ui.label(")");
+
+                    // スペースを元に戻す
+                    ui.spacing_mut().item_spacing.x = spacing;
+                }
+                OptimizeStatus::Error(e) => {
+                    ui.label(text_color("❌", Color32::RED, None));
+                    ui.label(path.file_name());
+                    ui.label(text_color(e, Color32::RED, Some(11.0)));
                 }
             }
-            Err(_) => {}
+        });
+
+        if index < files.paths().len() - 1 {
+            ui.add(egui::Separator::default().spacing(4.0));
         }
     }
 }
